554932718e7b20a2afbe234c31fc02a5f14eeaa1
William Chettleburgh <zcs0@cdc.gov>
2026-04-27 13:51:42 -0400

    Use mapped impl in regular impl of find_k_repeating

--- src/search/k_repeating.rs
+++ src/search/k_repeating.rs
@@ -1,7 +1,4 @@
-use crate::{
-    iter_utils::SteppedWindows,
-    search::{position_by_byte_inner, position_by_byte_mapped_inner},
-};
+use crate::{iter_utils::SteppedWindows, search::position_by_byte_mapped_inner};
 use std::simd::prelude::*;
 
 /// Non-SIMD component of [`find_k_repeating`].
@@ -12,35 +9,9 @@ use std::simd::prelude::*;
 /// appropriately handle this case.
 #[inline]
 #[must_use]
+#[cfg(test)]
 pub(super) fn find_k_repeating_scalar(haystack: &[u8], needle: u8, size: usize) -> Option<usize> {
-    if size == 0 || size > haystack.len() {
-        return None;
-    }
-
-    let mut idx = size - 1;
-    let mut left_limit = 0usize;
-
-    'outer: while idx < haystack.len() {
-        if haystack[idx] == needle {
-            // Single match found: check backwards to see if it occurs `size` times
-            for prev_idx in (left_limit..idx).rev() {
-                if haystack[prev_idx] != needle {
-                    left_limit = idx + 1;
-                    idx = prev_idx + size;
-                    continue 'outer;
-                }
-            }
-
-            // No mismatch found: return the starting index of the match
-            return Some(idx + 1 - size);
-        }
-
-        // Mismatch found: advance idx to righmost position in leftmost possible match
-        left_limit = idx + 1;
-        idx += size;
-    }
-
-    None
+    find_k_repeating_mapped_scalar(haystack, needle, size, |x| x)
 }
 
 /// Non-SIMD component of [`find_k_repeating_mapped`].
@@ -82,29 +53,6 @@ where
     }
 
     None
-}
-
-/// This is the `size = 2` special case for the SIMD-acclerated portions of
-/// [`find_k_repeating`].
-#[inline]
-#[must_use]
-fn find_2_repeating_simd<const N: usize>(haystack: &[u8], needle: u8) -> Option<usize> {
-    let nv = Simd::<u8, N>::splat(needle);
-
-    // Get windows that are overlapping by 1
-    let mut chunks = SteppedWindows::new(haystack, N - 1);
-
-    for (i, chunk) in chunks.by_ref().copied().map(Simd::from_array).enumerate() {
-        let mut mask = nv.simd_eq(chunk).to_bitmask();
-        mask = mask & (mask >> 1);
-
-        if mask > 0 {
-            return Some(i * (N - 1) + mask.trailing_zeros() as usize);
-        }
-    }
-
-    let rem = chunks.final_partial_window();
-    find_k_repeating_scalar(rem, needle, 2).map(|found| (haystack.len() - rem.len()) + found)
 }
 
 /// This is the `size = 2` special case for the SIMD-acclerated portions of
@@ -159,90 +107,7 @@ where
 #[allow(clippy::cast_possible_truncation)]
 #[cfg_attr(feature = "multiversion", multiversion::multiversion(targets = "simd"))]
 pub fn find_k_repeating<const N: usize>(haystack: &[u8], needle: u8, size: usize) -> Option<usize> {
-    const SCALAR_THRESHOLD: usize = 21;
-
-    // TODO: fix when we have better const generics
-    const {
-        assert!(N > 2, "`find_k_repeating_simd` requires N > 2");
-    };
-
-    if size == 0 || size > haystack.len() {
-        return None;
-    } else if size == 1 {
-        return position_by_byte_inner::<N>(haystack, needle);
-    } else if size == 2 {
-        return find_2_repeating_simd::<N>(haystack, needle);
-    }
-
-    // Fall back to Scalar algorithm when it isn't worth it
-    if haystack.len() < N * 2 || size > SCALAR_THRESHOLD {
-        return find_k_repeating_scalar(haystack, needle, size);
-    }
-
-    let nv = Simd::<u8, N>::splat(needle);
-    let mut chunks = SteppedWindows::new(haystack, N - 2);
-    let mut running_total = 0;
-
-    for (i, chunk) in chunks.by_ref().copied().map(Simd::from_array).enumerate() {
-        let mut mask = nv.simd_eq(chunk).to_bitmask();
-        mask = mask & (mask >> 1) & (mask >> 2);
-
-        if mask == 0 {
-            running_total = 0;
-            continue;
-        }
-
-        let reset = mask & (1 << (N - 3)) == 0;
-        let mut bit_offset = 0;
-        if mask & 1 > 0 {
-            let previous = running_total;
-            bit_offset = mask.trailing_ones();
-            mask >>= bit_offset;
-
-            // CORRECTNESS: `trailing_ones()` cannot be larger than 64, which is
-            // much smaller than the `usize::MAX` of hypothetical 16-bit `usize`
-            // even though the value returned uses a `u32`.
-            running_total += bit_offset as usize;
-
-            if running_total >= size - 2 {
-                return Some(i * (N - 2) - previous);
-            }
-        }
-
-        while mask > 0 {
-            let skip_count = mask.trailing_zeros();
-            mask >>= skip_count;
-            bit_offset += skip_count;
-
-            // Restart running total
-            running_total = mask.trailing_ones() as usize;
-            mask >>= running_total;
-
-            if running_total >= size - 2 {
-                return Some(i * (N - 2) + bit_offset as usize);
-            }
-
-            bit_offset += running_total as u32;
-        }
-
-        if reset {
-            running_total = 0;
-        }
-    }
-
-    for (j, b) in chunks.final_partial_window().iter().copied().enumerate() {
-        if b == needle {
-            running_total += 1;
-        } else {
-            running_total = 0;
-        }
-
-        if running_total == size {
-            return Some(haystack.len() - chunks.final_partial_window().len() + j - (size - 1));
-        }
-    }
-
-    None
+    find_k_repeating_inner::<N>(haystack, needle, size)
 }
 
 /// Given a haystack, needle and the number of times the needle repeats itself
@@ -270,6 +135,28 @@ pub fn find_k_repeating<const N: usize>(haystack: &[u8], needle: u8, size: usize
 #[allow(clippy::cast_possible_truncation)]
 #[cfg_attr(feature = "multiversion", multiversion::multiversion(targets = "simd"))]
 pub fn find_k_repeating_mapped<const N: usize, S, B>(
+    haystack: &[u8], needle: u8, size: usize, simd_transform: S, byte_transform: B,
+) -> Option<usize>
+where
+    S: Fn(Simd<u8, N>) -> Simd<u8, N>,
+    B: Fn(u8) -> u8, {
+    find_k_repeating_mapped_inner(haystack, needle, size, simd_transform, byte_transform)
+}
+
+/// Similar to [`find_k_repeating`], but without multiversioning (for use as a
+/// helper function inside other multiversioned functions).
+#[inline]
+#[must_use]
+pub(crate) fn find_k_repeating_inner<const N: usize>(haystack: &[u8], needle: u8, size: usize) -> Option<usize> {
+    find_k_repeating_mapped_inner(haystack, needle, size, |v: Simd<u8, N>| v, |b| b)
+}
+
+/// Similar to [`find_k_repeating_mapped`], but without multiversioning (for use
+/// as a helper function inside other multiversioned functions).
+#[inline]
+#[must_use]
+#[allow(clippy::cast_possible_truncation)]
+pub(crate) fn find_k_repeating_mapped_inner<const N: usize, S, B>(
     haystack: &[u8], needle: u8, size: usize, simd_transform: S, byte_transform: B,
 ) -> Option<usize>
 where
