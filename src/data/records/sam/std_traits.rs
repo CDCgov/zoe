4cfab2345624520b52b46916de9bed2accb4612f
William Chettleburgh <zcs0@cdc.gov>
2026-09-08 11:40:57 -0400

    Defer the display impl for SamData and SamDataViewMut to SamDataView

--- src/data/records/sam/std_traits.rs
+++ src/data/records/sam/std_traits.rs
@@ -3,43 +3,7 @@ use super::*;
 impl std::fmt::Display for SamData {
     #[inline]
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let SamData {
-            qname,
-            flag,
-            rname,
-            pos,
-            mapq,
-            cigar,
-            rnext,
-            pnext,
-            tlen,
-            seq,
-            qual,
-            opt_fields,
-        } = self;
-
-        let seq = if is_missing_sam_field(seq) {
-            NucleotidesView::from(b"*")
-        } else {
-            seq.as_view()
-        };
-        let qual = if is_missing_sam_field(qual) {
-            // Safety: `b"*"` is graphic ASCII, which satisfies
-            // `QualityScoresView`'s byte invariant.
-            unsafe { QualityScoresView::from_bytes_unchecked(b"*") }
-        } else {
-            qual.as_view()
-        };
-
-        write!(
-            f,
-            "{qname}\t{flag}\t{rname}\t{pos}\t{mapq}\t{cigar}\t{rnext}\t{pnext}\t{tlen}\t{seq}\t{qual}"
-        )?;
-
-        for opt_field in &opt_fields.0 {
-            write!(f, "\t{opt_field}")?;
-        }
-        Ok(())
+        self.as_view().fmt(f)
     }
 }
 
@@ -90,37 +54,7 @@ impl std::fmt::Display for SamDataView<'_> {
 impl std::fmt::Display for SamDataViewMut<'_> {
     #[inline]
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let SamDataViewMut {
-            qname,
-            flag,
-            rname,
-            pos,
-            mapq,
-            cigar,
-            rnext,
-            pnext,
-            tlen,
-            seq,
-            qual,
-        } = self;
-
-        let seq = if is_missing_sam_field(seq) {
-            NucleotidesView::from(b"*")
-        } else {
-            seq.as_view()
-        };
-        let qual = if is_missing_sam_field(qual) {
-            // Safety: `b"*"` is graphic ASCII, which satisfies
-            // `QualityScoresView`'s byte invariant.
-            unsafe { QualityScoresView::from_bytes_unchecked(b"*") }
-        } else {
-            qual.as_view()
-        };
-
-        write!(
-            f,
-            "{qname}\t{flag}\t{rname}\t{pos}\t{mapq}\t{cigar}\t{rnext}\t{pnext}\t{tlen}\t{seq}\t{qual}"
-        )
+        self.as_view().fmt(f)
     }
 }
 
